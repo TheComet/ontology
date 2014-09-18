21458c8428e5e1a563194a9132cc11ec5d83ba41
TheComet <alex.murray@gmx.ch>
2014-09-18 21:26:30 +0200

    Added google test

---
+++ test/test/src/test_SystemManager.cpp
@@ -0,0 +1,17 @@
+// ----------------------------------------------------------------------------
+// include files
+
+#include <gtest/gtest.h>
+#include <ontology/ontology/Ontology.hpp>
+
+// ----------------------------------------------------------------------------
+// test fixture
+
+#define NAME TestSystemManager
+
+struct System1 : public
+
+// ----------------------------------------------------------------------------
+// tests
+
+TEST(NAME, AddSystems)
