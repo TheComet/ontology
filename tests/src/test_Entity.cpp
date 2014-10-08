5aafed18e44f3a071b07117c9f7b51da9d9f543c
TheComet <alex.murray@gmx.ch>
2014-10-08 18:11:33 +0200

    Entity unit test for destructor

--- tests/src/test_Entity.cpp
+++ tests/src/test_Entity.cpp
@@ -6,21 +6,8 @@
 using namespace Ontology;
 
 // ----------------------------------------------------------------------------
-// test fixture
+// test fixtures
 // ----------------------------------------------------------------------------
-
-struct MockEntityManager : public EntityManagerInterface
-{
-    Entity& createEntity(const char*) { return entity; }
-    MOCK_METHOD1(destroyEntity, void(Entity&));
-    MOCK_METHOD1(destroyEntities, void(const char*));
-    MOCK_METHOD0(destroyAllEntities, void());
-    MOCK_CONST_METHOD2(informAddComponent, void(Entity&, const Component*));
-    MOCK_CONST_METHOD2(informRemoveComponent, void(Entity&, const Component*));
-
-    MockEntityManager() : entity("invalid_entity", this) {}
-    Entity entity;
-};
 
 struct TestComponent : public Component
 {
@@ -36,41 +23,72 @@ struct NonExistingComponent : public Component
 {
 };
 
+// insert ourselves between System events so the components can be upcast
+// to TestComponent. This is so they can be tested.
+class MockEntityManagerHelper : public EntityManagerInterface
+{
+    void informAddComponent(Entity& e, const Component* c) const override
+    { this->informAddComponentHelper(e, static_cast<const TestComponent*>(c)); }
+    void informRemoveComponent(Entity& e, const Component* c) const override
+    { this->informRemoveComponentHelper(e, static_cast<const TestComponent*>(c)); }
+
+    // WARNING: DO NOT CALL THIS FUNCTION - It is required to be implemented
+    // by the base class (abstract function) but doesn't do what it is intended
+    // to. This is only here to make the compiler happy.
+    Entity& createEntity(const char* name) override { Entity("invalid_entity", this); }
+
+public:
+    virtual ~MockEntityManagerHelper() {}
+    virtual void informAddComponentHelper(Entity&, const TestComponent*) const = 0;
+    virtual void informRemoveComponentHelper(Entity&, const TestComponent*) const = 0;
+};
+
+struct MockEntityManager : public MockEntityManagerHelper
+{
+    MOCK_METHOD1(destroyEntity, void(Entity&));
+    MOCK_METHOD1(destroyEntities, void(const char*));
+    MOCK_METHOD0(destroyAllEntities, void());
+    MOCK_CONST_METHOD2(informAddComponentHelper, void(Entity&, const TestComponent*));
+    MOCK_CONST_METHOD2(informRemoveComponentHelper, void(Entity&, const TestComponent*));
+
+};
+
 // ----------------------------------------------------------------------------
 // tests
 // ----------------------------------------------------------------------------
 
 TEST(NAME, CreateEntityWithName)
 {
-    MockEntityManager entityManager;
-    Entity entity("entity1", &entityManager);
+    MockEntityManager em;
+    Entity entity("entity1", &em);
     ASSERT_EQ(std::string("entity1"), std::string(entity.getName()));
 }
 
 TEST(NAME, AddingAndRemovingComponentsInformsEntityManager)
 {
-    MockEntityManager entityManager;
-    Entity entity("entity", &entityManager);
+    MockEntityManager em;
+    Entity entity("entity", &em);
 
-    EXPECT_CALL(entityManager, informAddComponent(testing::_, testing::_))
+    EXPECT_CALL(em, informAddComponentHelper(testing::_, testing::Pointee(TestComponent(2, 3))))
         .Times(1);
-    EXPECT_CALL(entityManager, informRemoveComponent(testing::_, testing::_))
+    EXPECT_CALL(em, informRemoveComponentHelper(testing::_, testing::Pointee(TestComponent(12, 3))))
         .Times(1);
 
     entity.addComponent<TestComponent>(2, 3);
+    entity.getComponent<TestComponent>().x = 12;
     entity.removeComponent<TestComponent>();
 }
 
 TEST(NAME, AddingTwoComponentsOfTheSameTypeCausesDeath)
 {
-    MockEntityManager entityManager;
-    Entity entity("entity", &entityManager);
+    MockEntityManager em;
+    Entity entity("entity", &em);
 
     // uninteresting calls
-    EXPECT_CALL(entityManager, informRemoveComponent(testing::_, testing::_));
+    EXPECT_CALL(em, informRemoveComponentHelper(testing::_, testing::_));
 
     // interesting calls
-    EXPECT_CALL(entityManager, informAddComponent(testing::_, testing::_))
+    EXPECT_CALL(em, informAddComponentHelper(testing::_, testing::Pointee(TestComponent(2, 3))))
         .Times(1);
 
     entity.addComponent<TestComponent>(2, 3);
@@ -80,17 +98,29 @@ TEST(NAME, AddingTwoComponentsOfTheSameTypeCausesDeath)
 
 TEST(NAME, GettingNonExistingComponentsCausesDeath)
 {
-    MockEntityManager entityManager;
-    Entity entity("entity", &entityManager);
+    MockEntityManager em;
+    Entity entity("entity", &em);
 
     // uninteresting calls
-    EXPECT_CALL(entityManager, informRemoveComponent(testing::_, testing::_));
-
-    // interesint calls
-    EXPECT_CALL(entityManager, informAddComponent(testing::_, testing::_))
-        .Times(1);
+    EXPECT_CALL(em, informRemoveComponentHelper(testing::_, testing::_));
+    EXPECT_CALL(em, informAddComponentHelper(testing::_, testing::_));
 
     entity.addComponent<TestComponent>(2, 3);
     ASSERT_EQ(TestComponent(2, 3), entity.getComponent<TestComponent>());
     ASSERT_DEATH(entity.getComponent<NonExistingComponent>(), "");
+}
+
+TEST(NAME, EntityDestructionInformsEntityManagerAboutComponentRemoval)
+{
+    MockEntityManager em;
+    Entity entity("entity", &em);
+
+    // uninteresting calls
+    EXPECT_CALL(em, informAddComponentHelper(testing::_, testing::_));
+
+    // interesint calls
+    EXPECT_CALL(em, informRemoveComponentHelper(testing::_, testing::Pointee(TestComponent(2, 4))))
+        .Times(1);
+
+    entity.addComponent<TestComponent>(2, 4);
 }
